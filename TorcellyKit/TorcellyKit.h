bf22896b11a914b7500c078d55c85516e3007ac7
Fernando Torcelly <fernando@torcelly.com>
2017-08-28 02:44:33 +0100

    Create Xcode project

---
+++ TorcellyKit/TorcellyKit.h
@@ -0,0 +1,19 @@
+//
+//  TorcellyKit.h
+//  TorcellyKit
+//
+//  Created by Fernando Torcelly on 28/8/17.
+//  Copyright © 2017 Fernando Torcelly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TorcellyKit.
+FOUNDATION_EXPORT double TorcellyKitVersionNumber;
+
+//! Project version string for TorcellyKit.
+FOUNDATION_EXPORT const unsigned char TorcellyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TorcellyKit/PublicHeader.h>
+
+
