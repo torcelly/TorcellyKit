9414049a65446617c22dfc485ecc862bdaec674c
Fernando Torcelly <fernando@torcelly.com>
2018-03-11 21:15:45 +0000

    Add main cells for tables and collection views

---
+++ TorcellyKit/Cells/MainCollectionViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  MainCollectionViewCell.swift
+//  Squire Console
+//
+//  Created by Fernando Torcelly on 28/8/17.
+//  Copyright © 2017 Fernando Torcelly. All rights reserved.
+//
+
+import UIKit
+
+open class MainCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Initializers
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.build()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Identifier
+    
+    public class func identifier() -> String {
+        return NSStringFromClass(self)
+    }
+    
+    // MARK: - Interface
+    
+    open func build() {}
+}
---
+++ TorcellyKit/Cells/MainTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  MainTableViewCell.swift
+//  Backstage
+//
+//  Created by Fernando Torcelly on 14/8/17.
+//  Copyright © 2017 Fernando Torcelly. All rights reserved.
+//
+
+import UIKit
+
+open class MainTableViewCell: UITableViewCell {
+    
+    // MARK: - Initializers
+    
+    public override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: MainTableViewCell.identifier())
+        self.build()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Identifier
+    
+    public class func identifier() -> String {
+        return NSStringFromClass(self)
+    }
+    
+    // MARK: - Interface
+    
+    open func build() {}
+}
